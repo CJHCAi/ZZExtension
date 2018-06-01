7db64b2dbf4b8d150e3e747a0457bd8cfab58a4e
刘猛 <ly@ly.local>
2018-06-02 00:17:42 +0800

    略微优化了demo!

--- ZZExtension/ZZExtension/Demo/ZZTabbarController/ZZTabbarController.m
+++ ZZExtension/ZZExtension/Demo/ZZTabbarController/ZZTabbarController.m
@@ -14,7 +14,6 @@
 
 //容器数组
 @property(nonatomic,strong)NSArray          *imageNames;
-@property(nonatomic,strong)NSArray          *titleNames;
 @property(nonatomic,strong)NSArray          *selectedImageNames;
 
 //tabbar上的控制器
@@ -44,23 +43,46 @@
 #pragma mark - 这里的demo是举例说明怎么添加一个超出tabbar的按钮,按钮你自己写!
 -(void)demo{
     
-    //实现:设置超出的按钮(其实就是写一个按钮覆盖到self.tabBar上)
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [self.tabBar addSubview:button];
-    [button setImage:[UIImage imageNamed:@"huibaowdj"] forState:(UIControlStateNormal)];
-    [button setImage:[UIImage imageNamed:@"huibaodj"] forState:(UIControlStateSelected)];
-    button.backgroundColor = [UIColor darkGrayColor];
-    button.sd_layout.centerXEqualToView(self.tabBar).bottomSpaceToView(self.tabBar, ZZSafeAreaBottomHeight)
-    .widthIs(ZZWidth / self.viewControllers.count)
-    .heightIs(64);
+    //1.这里是获取一个按钮,你自己写样式就行了,我这里通过type给你几个不同的选项看效果!
+    UIButton *button = [self getButtonWithType:1];
     
-    //核心代码:传入的按钮就直接作为中间的按钮了,给了一个回调,可以获取按钮的点击事件,请注意block的循环引用!
-    [self.tabBar zz_setCenterButtonWithButton:button selectIndexWhenThisButtonClick:1 callBack:^{
-        
-    }];
+    //核心代码
+    //普通效果:传入的按钮就直接作为中间的按钮了,给了一个回调,可以获取按钮的点击事件,请注意block的循环引用!
+    [self.tabBar zz_setCenterButtonWithButton:button selectIndexWhenThisButtonClick:1 callBack:nil];
+    
+//    //push效果:一样的参数,传入selectIndexWhenThisButtonClick<0的值,不会改变选中的item,但仍然会回调callBack!通过callback可以完成你想要的操作,如present出一个控制器,最好处理一下block的循环引用.
+//    __weak typeof (self)weakSelf = self;
+//    [self.tabBar zz_setCenterButtonWithButton:button selectIndexWhenThisButtonClick:-1 callBack:^{
+//        ZZViewController *vc = [[ZZViewController alloc] init];
+//        vc.view.backgroundColor = [UIColor darkGrayColor];
+//        [weakSelf presentViewController:vc animated:YES completion:nil];
+//    }];
     
 }
 
+-(UIButton *)getButtonWithType:(int)type{
+    UIButton *button;
+    if (type == 1) {
+        button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [self.tabBar addSubview:button];
+        [button setImage:[UIImage imageNamed:@"huibaowdj"] forState:(UIControlStateNormal)];
+        [button setImage:[UIImage imageNamed:@"huibaodj"] forState:(UIControlStateSelected)];
+        button.backgroundColor = [UIColor darkGrayColor];
+        button.sd_layout.centerXEqualToView(self.tabBar).bottomSpaceToView(self.tabBar, ZZSafeAreaBottomHeight)
+        .widthIs(ZZWidth / self.viewControllers.count)
+        .heightIs(64);
+    }else if (type == 2){
+        button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [self.tabBar addSubview:button];
+        [button setImage:[UIImage imageNamed:@"huibaowdj"] forState:(UIControlStateNormal)];
+        [button setImage:[UIImage imageNamed:@"huibaodj"] forState:(UIControlStateSelected)];
+        button.backgroundColor = [UIColor clearColor];
+        button.sd_layout.centerXEqualToView(self.tabBar).bottomSpaceToView(self.tabBar, ZZSafeAreaBottomHeight)
+        .widthIs(ZZWidth / self.viewControllers.count)
+        .heightIs(64);
+    }
+    return button;
+}
 
 #pragma mark - tabbar高度适配iPhone X,并非框架用法必须,但屏幕适配你也少不了!
 -(void)viewWillLayoutSubviews{
@@ -73,7 +95,6 @@
 
 #pragma mark - tabbarController的基础设置,这里你可以有你自己的设置.
 -(void)setupTabbarController{
-    self.titleNames = @[@"首页",@"",@"我的"];
     self.imageNames = @[@"icon_information1",@"",@"icon_my1"];
     self.selectedImageNames = @[@"icon_information2",@"",@"icon_my2"];
     
@@ -89,7 +110,7 @@
     //地图控制器
     ZZViewController *mapVC = [[ZZViewController alloc] init];
     mapVC.view.backgroundColor = [UIColor whiteColor];
-    mapVC.title = @"地图";
+    [mapVC.navigationItem setTitle:@"地图"];
     UINavigationController *mapNav = [[UINavigationController alloc] initWithRootViewController:mapVC];
     self.homeVC = mapVC;[vcArr addObject:mapNav];
     
@@ -101,7 +122,7 @@
     self.profileVC = profileVC;[vcArr addObject:profileNav];
     
     for (int i = 0; i < vcArr.count; i ++) {
-        UIViewController *vc = vcArr[i];//vc.tabBarItem.title = self.titleNames[i];
+        UIViewController *vc = vcArr[i];
         vc.tabBarItem.image = [[UIImage imageNamed:self.imageNames[i]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
         vc.tabBarItem.selectedImage = [[UIImage imageNamed:self.selectedImageNames[i]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     }
--- ZZExtension/ZZExtension/Demo/ZZViewController/ZZViewController.m
+++ ZZExtension/ZZExtension/Demo/ZZViewController/ZZViewController.m
@@ -19,7 +19,19 @@
     
 }
 
+#pragma mark - 这里的代码没有看的必要,就是为了demo的效果展示做的一些容错.
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    //如果不在导航控制器则直接dismiss,主要是为了demo看效果,你可能用不到,不用管这个逻辑
+    if (!self.navigationController) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+        return;
+    }
+    
+    //每个导航控制器只push一次,就是为了让你看效果,不用理解什么逻辑.
+    if (self.navigationController.viewControllers.count > 2) {
+        return;
+    }
+    
     ZZViewController *vc = [[ZZViewController alloc] init];
     
     if ([self.title isEqualToString:@"首页"]) {//首页的隐藏tabbar,其他的不隐藏(用来看效果的,证明可用,一般不会用到这个逻辑)
@@ -27,6 +39,7 @@
     }
     
     vc.view.backgroundColor = [UIColor blueColor];
+    
     [self.navigationController pushViewController:vc animated:YES];
 }
 

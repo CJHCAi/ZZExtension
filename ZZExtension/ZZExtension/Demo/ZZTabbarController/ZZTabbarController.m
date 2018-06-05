404f2d1a5bb34cea4424f390989ae72a46e4edf7
iOS-ZZ <1156858877@qq.com>
2018-06-05 21:37:18 +0800

    更新了readme！

--- ZZExtension/ZZExtension/Demo/ZZTabbarController/ZZTabbarController.m
+++ ZZExtension/ZZExtension/Demo/ZZTabbarController/ZZTabbarController.m
@@ -75,7 +75,7 @@
     //2.核心代码:普通效果:传入的按钮就直接作为中间的按钮了,给了一个回调,可以获取按钮的点击事件,请注意block的循环引用!
     [self.tabBar zz_setCenterButtonWithButton:self.centerButton selectIndexWhenThisButtonClick:1 callBack:nil];
     
-    /**3.核心代码:present效果:一样的参数,传入selectIndexWhenThisButtonClick<0的值,不会改变选中的item,但仍然会回调callBack!(点击查看)
+    /**3.核心代码:present效果:传入selectIndexWhenThisButtonClick<0的值,不会改变选中的item!
      __weak typeof (self)weakSelf = self;
      [self.tabBar zz_setCenterButtonWithButton:button selectIndexWhenThisButtonClick:-1 callBack:^{
      ZZViewController *vc = [[ZZViewController alloc] init];
